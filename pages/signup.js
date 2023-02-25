2152dc8cbef72a208a9a61e75d67dc4d69c16955
Evans <work.evans020@gmail.com>
2023-02-26 00:11:06 +0300

    removed a /requests/userRequests import from signup

--- pages/signup.js
+++ pages/signup.js
@@ -4,7 +4,6 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useEffect, useRef } from "react";
-import UserRequests from "../requests/UserRequets";
 import Config from "../Config";
 import axios from "axios";
 
